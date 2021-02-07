00ec2661b5684d96b00a8b6cc453fd49c9bfab0d
Karen <devkarenc@gmail.com>
2021-02-06 19:09:00 -0500

    Display default recipes and change page styling

--- index.js
+++ index.js
@@ -4,12 +4,65 @@ const myRecipes = [
     time: "20 minutes",
     difficulty: "Easy",
     ingredients:
-      "Butter, Onion, Garlic, Flour, Tomato, Tomato Sauce, Chicken Stock, Sugar, Salt, Pepper",
+      "Butter, Onion, Garlic, Flour, Tomato, Tomato sauce, Chicken stock, Sugar, Salt, Pepper",
     instructionsLink: "https://www.simplyrecipes.com/recipes/tomato_soup/",
+    haveCooked: true,
+  },
+  {
+    name: "Chicken Mac and Cheese",
+    time: "45 minutes",
+    difficulty: "Easy",
+    ingredients:
+      "Macaroni, Onion, Garlic, Butter, Flour, Milk, Cheddar cheese, Cooked chicken, Salt, Pepper",
+    instructionsLink:
+      "https://www.simplyrecipes.com/recipes/easy_chicken_mac_and_cheese/",
+    haveCooked: false,
+  },
+  {
+    name: "Greek Salad",
+    time: "15 minutes",
+    difficulty: "Easy",
+    ingredients:
+      "Olive oil, Lemon juice, Garlic, Vinegar, Oregano, Pepper, Tomatoes, Cucumber, Onion, Bell pepper, Olives, Feta Cheese",
+    instructionsLink: "https://www.simplyrecipes.com/recipes/dads_greek_salad/",
     haveCooked: false,
   },
 ];
 
+function displayDefaultRecipes() {
+  const recipesContainer = document.querySelector(".recipes-container");
+  myRecipes.forEach((defaultRecipe) => {
+    const recipeCard = document.createElement("div");
+    recipeCard.classList.add("recipe-card");
+    const recipeName = document.createElement("div");
+    // const recipePic = document.createElement("div");
+    const recipeTime = document.createElement("div");
+    const recipeDifficulty = document.createElement("div");
+    const recipeIngredients = document.createElement("div");
+    const recipeInstructions = document.createElement("div");
+    const recipeCooked = document.createElement("div");
+    recipeName.textContent = defaultRecipe.name;
+    recipeTime.textContent = defaultRecipe.time;
+    recipeDifficulty.textContent = defaultRecipe.difficulty;
+    recipeIngredients.textContent = defaultRecipe.ingredients;
+    recipeInstructions.textContent = defaultRecipe.instructionsLink;
+    recipeCooked.textContent = defaultRecipe.haveCooked;
+    recipeCard.append(
+      recipeName,
+      // recipePic,
+      recipeTime,
+      recipeDifficulty,
+      recipeIngredients,
+      recipeInstructions,
+      recipeCooked
+    );
+    recipesContainer.append(recipeCard);
+  });
+}
+
+// displayDefaultRecipes();
+
+// Recipe Constructor Function
 function Recipe(
   name,
   time,
@@ -23,7 +76,7 @@ function Recipe(
   this.difficulty = difficulty;
   this.ingredients = ingredients;
   this.instructionsLink = instructionsLink;
-  this.haveCooked = false;
+  this.haveCooked = haveCooked;
 }
 
 function openModal() {
@@ -57,6 +110,7 @@ function saveRecipe() {
     haveCooked
   );
   myRecipes.push(newRecipe);
+  displayRecipes();
   console.log(myRecipes);
 }
 
@@ -68,3 +122,37 @@ function addRecipes() {
 }
 
 addRecipes();
+
+function displayRecipes() {
+  const recipesContainer = document.querySelector(".recipes-container");
+  myRecipes.forEach((recipe) => {
+    // if (!myRecipes.includes(recipe)) {
+    // console.log(recipe);
+    const recipeCard = document.createElement("div");
+    recipeCard.classList.add("recipe-card");
+    const recipeName = document.createElement("div");
+    // const recipePic = document.createElement("div");
+    const recipeTime = document.createElement("div");
+    const recipeDifficulty = document.createElement("div");
+    const recipeIngredients = document.createElement("div");
+    const recipeInstructions = document.createElement("div");
+    const recipeCooked = document.createElement("div");
+    recipeName.textContent = recipe.name;
+    recipeTime.textContent = recipe.time;
+    recipeDifficulty.textContent = recipe.difficulty;
+    recipeIngredients.textContent = recipe.ingredients;
+    recipeInstructions.textContent = recipe.instructionsLink;
+    recipeCooked.textContent = recipe.haveCooked;
+    recipeCard.append(
+      recipeName,
+      // recipePic,
+      recipeTime,
+      recipeDifficulty,
+      recipeIngredients,
+      recipeInstructions,
+      recipeCooked
+    );
+    recipesContainer.append(recipeCard);
+    // }
+  });
+}
